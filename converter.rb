a155d62c5e3ee86395adb5f09a117111b7d72dd1
burningTyger <m8r-0478r31@safetymail.info>
2011-05-27 00:09:22 +0200

    initial commit

---
+++ converter.rb
@@ -0,0 +1,51 @@
+require 'mongo_mapper'
+
+MongoMapper.database = 'testing'
+
+class Lemma
+  include MongoMapper::Document
+  key :lemma, String
+  key :translation_ids, Array
+  many :translations, :in => :translation_ids
+  timestamps!
+end
+
+class Translation
+  include MongoMapper::Document
+  key :source, String
+  key :target, String
+  timestamps!
+end
+
+Lemma.collection.remove
+Translation.collection.remove
+Lemma.ensure_index(:lemma)
+
+error = {}
+Dir.glob('../farhang/*.txt').each do |ff|
+  File.open(ff, 'r') do |f|
+    lemma = nil
+    f.each_line do |l|
+      source, target = l.split(';')
+      begin
+        source.strip!
+        target.strip!
+      rescue
+        error["#{ff.basename}: #{f.lineno}"] = l
+      end
+      unless source.start_with?('- ')
+        lemma = Lemma.create( :lemma => source )
+        trans = Translation.create( :source => source, :target => target )
+        lemma.translations << trans
+        lemma.save
+      else
+        source.sub!('- ', '')
+        trans = Translation.create( :source => source, :target => target )
+        lemma.translations << trans
+        lemma.save
+      end
+    end
+  end
+end
+
+p error.to_s
