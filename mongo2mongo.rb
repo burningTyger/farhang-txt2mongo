6177c437c0daddf1e14fccd87f497b071e2d402f
burningTyger <b6tyger@gmail.com>
2014-02-20 21:26:42 +0100

    fix for databaseslugging

--- mongo2mongo.rb
+++ mongo2mongo.rb
@@ -3,17 +3,29 @@ if ARGV[0]
   require 'mongo_mapper'
   require 'unicode'
   require 'babosa'
+  require 'mm-sluggable'
 
   MongoMapper.database = ARGV[0]
 
   class Lemma
     include MongoMapper::Document
+    plugin MongoMapper::Plugins::Sluggable
     key :lemma, String, :unique => true, :required => true, :index => true
-    key :slug, String, :unique => true
+    key :slug, String, :unique => true, :required => true
+    sluggable :lemma,
+              :method => :slug_hack,
+              :callback => :before_validation
   end
 
-  Lemma.find_each do |l|
-    l.set :slug => l.lemma.to_slug.clean.normalize(:transliterate => :german)
+  class String
+    def slug_hack
+      to_slug.clean.normalize(:transliterate => :german)
+    end
+  end
+
+  Lemma.find_each :batch_size => 500 do |l|
+    l.slug = nil
+    l.save!
   end
 end
 
